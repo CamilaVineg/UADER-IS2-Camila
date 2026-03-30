8ca0fccec0433b1e1ce787b243525a88fe8cdcbb
Camila Viñeg <vinegcamila27@gmail.com>
2026-03-30 00:54:54 -0300

    Se incorpora la segunda modificacion de rango.

---
+++ src/factorial/factorial_rango_2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# *-------------------------------------------------------------------------*
+# * factorial.py                                                            *
+# * calcula el factorial de un número                                       *
+# * Dr.P.E.Colla (c) 2022                                                   *
+# * Creative commons                                                        *
+# *-------------------------------------------------------------------------*
+
+import sys
+
+def factorial(num):
+    if num < 0:
+        print("Factorial de un número negativo no existe")
+        return None
+    elif num == 0:
+        return 1
+    else:
+        fact = 1
+        while num > 1:
+            fact *= num
+            num -= 1
+        return fact
+
+def procesar_rango(rango_str):
+    try:
+        # Caso "-hasta"
+        if rango_str.startswith("-"):
+            hasta = int(rango_str[1:])
+            return 1, hasta
+        # Caso "desde-"
+        elif rango_str.endswith("-"):
+            desde = int(rango_str[:-1])
+            return desde, 60
+        # Caso "desde-hasta"
+        else:
+            desde, hasta = rango_str.split("-")
+            return int(desde), int(hasta)
+    except ValueError:
+        print("Formato inválido. Use desde-hasta, -hasta o desde-")
+        sys.exit()
+
+if len(sys.argv) < 2:
+    rango_str = input("Debe informar un rango (ej: 4-8, -10, 5-): ")
+else:
+    rango_str = sys.argv[1]
+
+desde, hasta = procesar_rango(rango_str)
+
+
+for num in range(desde, hasta + 1):
+    print(f"Factorial {num}! es {factorial(num)}")
